c59addaa3c17811a9364e14cfa51a9c507d2c063
권병준 <onlymeal-macbook@Kwon-MacBook-Pro.local>
2017-12-20 22:28:08 +0900

    update for train

---
+++ trainer.py
@@ -0,0 +1,106 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+import tensorflow as tf
+
+import model
+import util
+
+SEED 			= model.SEED
+IMG_SIZE 		= model.IMG_SIZE
+NUM_CHANNELS 	= model.NUM_CHANNELS
+
+class network:
+	def __init__(self, x, y, param):
+		print("initialize trainer... "),
+		self.x = x
+		self.y = y
+
+		self.W = param["weight"]
+		self.B = param["bias"]
+		
+		self.train_epoch 		= param["train_epoch"]
+		self.base_learning_rate	= param["learning_rate"]
+		self.decay_rate			= param["decay_rate"]
+		self.fold 				= param["fold"]
+		self.train_batch_size 	= param["train_batch_size"]
+		self.valid_batch_size 	= param["valid_batch_size"]
+		self.display_step 		= param["display_step"]
+		self.model 				= param["model"]
+
+		self.log_file			= param["log_file"]
+
+		self.X 		= tf.placeholder(tf.float32, [None, IMG_SIZE, IMG_SIZE, NUM_CHANNELS])
+		self.Y 		= tf.placeholder(tf.float32, [None, 1])
+		self.Train 	= tf.placeholder(tf.bool)
+	
+		self.logits   = self.model(self.X, self.W, self.B, self.train_batch_size, self.Train)
+		self.evaluate = self.model(self.X, self.W, self.B, self.train_batch_size, self.Train)
+		print("Done")
+
+	def build_graph(self, fold_idx):
+		print("build graph... ")
+
+		x_train, self.y_train, x_valid, self.y_valid = util.split_set(self.x, self.y, fold_idx+1, self.fold)
+
+		print(x_train.shape, self.y_train.shape, x_valid.shape, self.y_valid.shape)
+
+		x_train, x_valid = util.norm_by_std_nan(x_train, x_valid)
+		print("dsa")
+		self.x_train = x_train.reshape(x_train.shape[0], IMG_SIZE, IMG_SIZE, NUM_CHANNELS)
+		self.x_valid = x_valid.reshape(x_valid.shape[0], IMG_SIZE, IMG_SIZE, NUM_CHANNELS)
+
+		self.num_samples_train = self.x_train.shape[0]
+		self.num_samples_valid = self.x_valid.shape[0]
+
+		batch = tf.Variable(0, dtype = tf.float32)
+		
+		self.learning_rate 	= tf.train.inverse_time_decay(
+			self.base_learning_rate,
+			batch * self.train_batch_size,
+			self.num_samples_train,
+			self.decay_rate
+			)
+		self.loss = tf.reduce_mean(tf.square(self.logits - self.Y))
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss, global_step=batch)
+
+		print("Done")
+
+	def update(self):
+		print("update model...")
+		for fold_idx in range(self.fold) :
+			util.print_file("fold = %d / %d ---" % (fold_idx+1, self.fold), self.log_file)
+			self.build_graph(fold_idx)
+			with tf.Session() as sess :
+				sess.run(tf.global_variables_initializer())
+
+				for epoch in range(self.train_epoch):
+					# Train phase
+					train_loss = 0.
+					train_total_batch = int(self.num_samples_train / self.train_batch_size)
+					for step in range(train_total_batch):
+						batch_x, batch_y = util.load_batch(self.x_train, self.y_train, self.train_batch_size, step+1)
+						feed_dict = {self.X : batch_x, self.Y : batch_y, self.Train : True}
+						_, batch_loss = sess.run([self.optimizer, self.loss], feed_dict = feed_dict)
+						train_loss += batch_loss
+					train_loss /= train_total_batch
+
+					# Vaidation phase
+					valid_loss = 0.
+					valid_total_batch = int(self.num_samples_valid / self.valid_batch_size)
+					pred = []
+					for step in range(valid_total_batch):
+						batch_x, batch_y = util.load_batch(self.x_valid, self.y_valid, self.valid_batch_size, step+1)
+						feed_dict = {self.X : batch_x, self.Y : batch_y, self.Train : False}
+						batch_loss = sess.run(self.loss, feed_dict = feed_dict)
+						pred.append(sess.run(self.evaluate, feed_dict = feed_dict))
+						valid_loss += batch_loss
+					valid_loss /= valid_total_batch
+
+					# Calculate R2
+					pred = np.array(pred).reshape(-1, 1)
+					valid_R2 = util.get_R2(pred, self.y_valid[:pred.shape[0]])					
+					
+					if (epoch+1) % self.display_step == 0 :
+						util.print_file("Epoch %03d | valid_R2: %.4f | train_loss: %.4f | valid_loss: %.4f"
+							%(epoch+1, valid_R2, train_loss, valid_loss), self.log_file)
+		print("train done!!")
---
+++ util.py
@@ -0,0 +1,87 @@
+# -*- coding:utf-8 -*-
+import numpy as np
+
+import model
+
+SEED         = model.SEED
+IMG_SIZE     = model.IMG_SIZE
+NUM_CHANNELS = model.NUM_CHANNELS
+
+def load_data(data_path, label_path):
+	#npz가 pickle보다 압축 3배 이상 좋아 용량을 덜 차지함.
+	#근데 그만큼 압축되어 있어서 메모리에 올리는 시간은 아직 비교해보지 못함
+	print(":::Loding data...")
+	data  = np.load(data_path)['x']
+	label = np.load(label_path)['y']
+	return data, label
+
+def print_file(data, f):
+    print (data)
+    f.write(data+"\n")
+    
+    return
+
+def get_R2(pred, y_valid):
+    return 1-(np.sum(np.square(y_valid - pred)) / np.sum(np.square(y_valid - np.mean(y_valid))))
+
+def split_set(data_set, label_set, fold, k):
+    """split train set and test set"""
+    
+    rest = int(data_set.shape[0] % k)
+    quo = int(data_set.shape[0] / k)
+    
+    if fold != k:
+        test_sect = [quo*(fold-1), quo*fold]
+    else :
+        test_sect = [quo*(fold-1), quo*fold+rest]
+    
+    x_train = []
+    y_train = []
+    x_test  = []
+    y_test  = []
+    
+    for i in range(data_set.shape[0]):
+        if (test_sect[0]<=i) & (i<test_sect[1]):
+            x_test.append(data_set[i])
+            y_test.append(label_set[i])
+        else:
+            x_train.append(data_set[i])
+            y_train.append(label_set[i])
+    
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_test  = np.array(x_test)
+    y_test  = np.array(y_test)
+    
+    return x_train, y_train, x_test, y_test
+
+def load_batch(x_train, y_train, batch_size, n):
+    
+    return x_train[batch_size*(n-1):batch_size*n,], y_train[batch_size*(n-1):batch_size*n,].reshape(batch_size, 1)
+    
+def norm_by_std_for_y(train):
+    mean = np.mean(train, 0)
+    std = np.std(train, 0) + 1e-8
+    
+    return (train - mean)/std, mean, std
+    
+def norm_by_std(train, val):
+    mean = np.mean(train, 0)
+    std = np.std(train, 0) + 1e-8
+    
+    return (train - mean)/std, (val - mean)/std
+
+def norm_by_std_nan(train, val):
+    mask = np.ma.array(train, mask=np.isnan(train))
+    mean = np.mean(mask, 0)
+    std = np.std(mask, 0)
+
+    train = (train - mean) / std
+    train = np.where(train == np.nan, 0, train)
+    train = np.nan_to_num(train)
+
+    val = (val-mean)/std
+    val = np.where(val == np.nan, 0, val)
+    val = np.nan_to_num(val)
+    
+    return train, val
