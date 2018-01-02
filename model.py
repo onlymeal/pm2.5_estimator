fix for batch normalization

model.py
@@ -2,6 +2,8 @@
 import numpy as np
 import tensorflow as tf
 
+name = 'old_batch_norm'
+
 # define constant
 DATA_PATH 		= "../00.data/v10_170713_5x5_dataset.npz"
 LABEL_PATH		= "../00.data/v10_170713_5x5_label.npz"
@@ -10,7 +12,7 @@ IMG_SIZE 		= 5
 NUM_CHANNELS 	= 74
 
 # define hyper-parameter for train
-TRAIN_EPOCH	 	 = 500
+TRAIN_EPOCH	 	 = 200
 LEARNING_RATE  	 = 0.0005
 DECAY_RATE 		 = 0.95
 FOLD 			 = 10
@@ -18,6 +20,9 @@ TRAIN_BATCH_SIZE = 100
 VALID_BATCH_SIZE = 100
 DISPLAY_STEP 	 = 1
 NOISE_STD 		 = 0.5
+
+BATCH_NORM_DECAY = 0.997
+BATCH_NORM_EPSILON = 1e-5
 
 # define hyper_parameter for networks
 n_hidden_1 = 1024
@@ -53,12 +58,16 @@ def model(X, W, B, batch_size, train_phase):
     X = Gaussian_noise(X, NOISE_STD)
     
     cv = tf.nn.conv2d(X, W['c1'], strides=[1,1,1,1], padding='VALID') + B['c1']
-    cv = batch_norm_old(cv, B['b1'], B['g1'], train_phase)
+    cv = batch_norm(cv, B['b1'], B['g1'], train_phase)
+    #cv = batch_norm_old(cv, B['b1'], B['g1'], train_phase)
+    #cv = batch_norm_new(cv, train_phase)
     cv = tf.nn.relu(cv)
     print(cv)
     
     cv = tf.nn.conv2d(cv, W['c2'], strides=[1,1,1,1], padding='VALID') + B['c2']
-    cv = batch_norm_old(cv, B['b2'], B['g2'], train_phase)
+    cv = batch_norm(cv, B['b2'], B['g2'], train_phase)
+    #cv = batch_norm_old(cv, B['b2'], B['g2'], train_phase)
+    #cv = batch_norm_new(cv, train_phase)
     cv = tf.nn.relu(cv)
     print(cv)
     
@@ -80,6 +89,11 @@ def Gaussian_noise(input_X, std=0.01):
     new_X = input_X + tf.random_normal(tf.shape(input_X), mean=0.0, stddev=std, dtype=tf.float32)
     return new_X
 
+def batch_norm_new(x, phase_train):
+    x = tf.layers.batch_normalization(inputs=x, axis=3, momentum=BATCH_NORM_DECAY, epsilon=BATCH_NORM_EPSILON, 
+        center=True, scale=True, training=phase_train, fused=True)
+    return x
+
 def batch_norm_old(x, bn_b, bn_g, phase_train):
     
     beta = bn_b
@@ -89,33 +103,19 @@ def batch_norm_old(x, bn_b, bn_g, phase_train):
     normed = tf.nn.batch_normalization(x, batch_mean, batch_var, beta, gamma, 1e-3)
     return normed
 
-def batch_norm(x, n_out, phase_train):
-    """
-    Batch normalization on convolutional maps.
-    Ref.: http://stackoverflow.com/questions/33949786/how-could-i-use-batch-normalization-in-tensorflow
-    Args:
-        x:           Tensor, 4D BHWD input maps
-        n_out:       integer, depth of input maps
-        phase_train: boolean tf.Varialbe, true indicates training phase
-        scope:       string, variable scope
-    Return:
-        normed:      batch-normalized maps
-    """
-    with tf.variable_scope('bn'):
-        beta = tf.Variable(tf.constant(0.0, shape=[n_out]),
-                                     name='beta', trainable=True)
-        gamma = tf.Variable(tf.constant(1.0, shape=[n_out]),
-                                      name='gamma', trainable=True)
-        batch_mean, batch_var = tf.nn.moments(x, [0,1,2], name='moments')
-        ema = tf.train.ExponentialMovingAverage(decay=0.5)
+def batch_norm(x, bn_b, bn_g, phase_train):
+    beta = bn_b
+    gamma = bn_g
+    batch_mean, batch_var = tf.nn.moments(x, [0, 1, 2], name='moments')
+    ema = tf.train.ExponentialMovingAverage(decay=0.5)
 
-        def mean_var_with_update():
-            ema_apply_op = ema.apply([batch_mean, batch_var])
-            with tf.control_dependencies([ema_apply_op]):
-                return tf.identity(batch_mean), tf.identity(batch_var)
+    def mean_var_with_update():
+        ema_apply_op = ema.apply([batch_mean, batch_var])
+        with tf.control_dependencies([ema_apply_op]):
+            return tf.identity(batch_mean), tf.identity(batch_var)
 
-        mean, var = tf.cond(phase_train,
-                            mean_var_with_update,
-                            lambda: (ema.average(batch_mean), ema.average(batch_var)))
-        normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
+    mean, var = tf.cond(phase_train,
+                        mean_var_with_update,
+                        lambda: (ema.average(batch_mean), ema.average(batch_var)))
+    normed = tf.nn.batch_normalization(x, mean, var, beta, gamma, 1e-3)
     return normed

trainer.py
@@ -34,7 +34,6 @@ class network:
 		self.Train 	= tf.placeholder(tf.bool, name="train_phase")
 	
 		self.logits   = self.model(self.X, self.W, self.B, self.train_batch_size, self.Train)
-		self.evaluate = self.model(self.X, self.W, self.B, self.train_batch_size, self.Train)
 		print("Done")
 
 	def build_graph(self, fold_idx):
@@ -94,7 +93,7 @@ class network:
 						batch_x, batch_y = util.load_batch(self.x_valid, self.y_valid, self.valid_batch_size, step+1)
 						feed_dict = {self.X : batch_x, self.Y : batch_y, self.Train : False}
 						batch_loss = sess.run(self.loss, feed_dict = feed_dict)
-						pred.append(sess.run(self.evaluate, feed_dict = feed_dict))
+						pred.append(sess.run(self.logits, feed_dict = feed_dict))
 						valid_loss += batch_loss
 					valid_loss /= valid_total_batch
 
@@ -106,6 +105,9 @@ class network:
 						util.print_file("Epoch %03d | valid_R2: %.4f | train_loss: %.4f | valid_loss: %.4f | learning rate: %.4f"
 							%(epoch+1, valid_R2, train_loss, valid_loss, learning_rate), self.log_file)
 					log_epoch.append([epoch+1, valid_R2, train_loss, valid_loss, learning_rate])
+					#for i in range(pred.shape[0]):
+					#	print(pred[i], self.y_valid[:pred.shape[0]][i])
+					#exit()
 			log_fold.append(log_epoch)
 		log_fold = np.array(log_fold)
 		util.get_result(log_fold, self.log_file)